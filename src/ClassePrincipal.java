34ce8dd0975cd47a729707a3c8e51d3226306e01
Nathalia Silva <nathaliasilvanov@gmail.com>
2019-04-17 20:20:12 -0300

    reorganizei tudo 17/04

--- src/ClassePrincipal.java
+++ src/ClassePrincipal.java
@@ -5,31 +5,55 @@ public class ClassePrincipal {
    
   public static void main(String [] args) {
        
-    Carro fusca = new Carro();
+    Carro carro = new Carro();
        
-       fusca.nome = "Fusca";
-       fusca.marca = "Volkswagem";
-       fusca.cor = "Azul";
-       fusca.fabricacao = "1556";
-       fusca.largura = "1,70 mm";
-       fusca.peso = "1300Kg";
-       
-       System.out.println ("Nome: " + fusca.nome);
-       System.out.println ("Marca: " + fusca.marca);
-       System.out.println ("Cor: " + fusca.cor);
-       System.out.println ("Fabricacao: " + fusca.fabricacao);
-       System.out.println ("Largura: " + fusca.largura);
-       System.out.println ("Peso: " + fusca.peso);
+      Scanner scan1 = new Scanner (System.in);
+    
+        System.out.print ("Nome: ");
+        carro.nome = scan1.next ();
+                
+        System.out.print ("Marca: ");
+        carro.marca = scan1.next ();
+                
+        System.out.print ("Cor: ");
+        carro.cor = scan1.next ();
+                
+        System.out.print ("Fabricacao: ");
+        carro.fabricacao = scan1.next ();
+                
+        System.out.print ("Largura: ");
+        carro.largura = scan1.next ();
+                
+        System.out.print ("Peso: ");
+        carro.peso = scan1.next ();
+    
+       System.out.println ("Nome: " + carro.nome);
+       System.out.println ("Marca: " + carro.marca);
+       System.out.println ("Cor: " + carro.cor);
+       System.out.println ("Fabricacao: " + carro.fabricacao);
+       System.out.println ("Largura: " + carro.largura);
+       System.out.println ("Peso: " + carro.peso);
        
     Carro brasilia = new Carro ();
-    
-      brasilia.nome="Brasilia";
-        brasilia.marca = "Volkswagen";
-        brasilia.cor="Amarelo";
-        brasilia.fabricacao="1973";
-        brasilia.largura="1,40 mm";
-        brasilia.peso="906kg";
         
+    System.out.print ("Nome: ");
+        brasilia.nome = scan1.next ();
+                
+        System.out.print ("Marca: ");
+        brasilia.marca = scan1.next ();
+                
+        System.out.print ("Cor: ");
+        brasilia.cor = scan1.next ();
+                
+        System.out.print ("Fabricacao: ");
+        brasilia.fabricacao = scan1.next ();
+                
+        System.out.print ("Largura: ");
+        brasilia.largura = scan1.next ();
+                
+        System.out.print ("Peso: ");
+        brasilia.peso = scan1.next ();
+    
         System.out.println("Nome:" + brasilia.nome) ;
         System.out.println("Marca:" + brasilia.marca) ;
         System.out.println("Cor:" + brasilia.cor) ;
@@ -37,27 +61,8 @@ public class ClassePrincipal {
         System.out.println("Largura:" + brasilia.largura) ;
         System.out.println("Peso:" + brasilia.peso) ;
                 
-    Carro carro01 = new Carro ();
     
-    Scanner scan1 = new Scanner (System.in);
-    
-        System.out.print ("Nome: ");
-        carro01.nome = scan1.next ();
-                
-        System.out.print ("Marca: ");
-                carro01.marca = scan1.next ();
-                
-        System.out.print ("Cor: ");
-        carro01.cor = scan1.next ();
-                
-        System.out.print ("Fabricacao: ");
-        carro01.fabricacao = scan1.next ();
-                
-        System.out.print ("Largura: ");
-        carro01.largura = scan1.next ();
-                
-        System.out.print ("Peso: ");
-        carro01.peso = scan1.next ();
+        
                 
                 
                 
